0add23caa26e0213ead251147e01d38f40feb33b
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-05 17:28:11 -0500

    Refactor express server middleware

--- student_practices/W10/D2/aa10-express-middleware/server/app.js
+++ student_practices/W10/D2/aa10-express-middleware/server/app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// Logger Middleware
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    console.log(`Logged ${req.method} ${req.originalUrl} - ${res.statusCode}`);
+  });
+  next();
+});
+
 // For testing purposes, GET /
-app.get('/', (req, res) => {
-  res.json("Express server running. No content provided at root level. Please use another route.");
+app.get("/", (req, res) => {
+  res.json(
+    "Express server running. No content provided at root level. Please use another route."
+  );
 });
 
 // For testing express.json middleware
-app.post('/test-json', (req, res, next) => {
+app.post("/test-json", (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
   res.json(req.body);
@@ -15,9 +27,22 @@ app.post('/test-json', (req, res, next) => {
 });
 
 // For testing express-async-errors
-app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+app.get("/test-error", async (req, res) => {
+  throw new Error("Hello World!");
+});
+
+// Resource Not Found Middleware
+app.use((req, res, next) => {
+  const error = new Error("The requested resource couldn't be found.");
+  error.statusCode = 404;
+  throw error;
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500);
+  res.json({ message: err.message });
 });
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
