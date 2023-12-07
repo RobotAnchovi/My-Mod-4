0a7d482cd284683943178987634e2cd501a10323
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-07 11:22:30 -0500

    Add tables for bands, modify musicians table for one-to-many relationship, and create instruments and musicians_instruments tables

--- student_practices/W10/D4/1-sql-create-related-tables/sql-practice/music.sql
+++ student_practices/W10/D4/1-sql-create-related-tables/sql-practice/music.sql
@@ -17,3 +17,30 @@ CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
 );
+--*====> Creating bands table <====
+CREATE TABLE bands (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+--*====> Modifying musicians table for one-to-many relationship with bands <====
+ALTER TABLE musicians ADD COLUMN band_id INTEGER;
+ALTER TABLE musicians ADD FOREIGN KEY (band_id) REFERENCES bands(id);
+
+--*====> Creating instruments table <====
+CREATE TABLE instruments (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+--*====> Creating join table for many-to-many relationship between musicians and instruments <====
+CREATE TABLE musicians_instruments (
+    musician_id INTEGER,
+    instrument_id INTEGER,
+    FOREIGN KEY (musician_id) REFERENCES musicians(id),
+    FOREIGN KEY (instrument_id) REFERENCES instruments(id)
+);
+
+.mode 'box';
+ SELECT * FROM bands;
+ SELECT * FROM musicians;
