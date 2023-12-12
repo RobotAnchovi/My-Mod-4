2e648a1bf22dc20bce9fc26734019b6d635841b2
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-12 16:48:16 -0500

    Complete Practice 5

--- student_practices/W11/D2/3-sequelize-seeders/server/db/seeders/20231212202418-fancy-colors.js
+++ student_practices/W11/D2/3-sequelize-seeders/server/db/seeders/20231212202418-fancy-colors.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const customDate = new Date("2023-01-01"); // Example date
+    const customDate = new Date("2023-01-01");
     await queryInterface.bulkInsert("Colors", [
       { name: "aqua", createdAt: customDate, updatedAt: customDate },
       { name: "fuchsia", createdAt: customDate, updatedAt: customDate },
--- student_practices/W11/D2/4-sequelize-validations-and-constraints/server/db/models/color.js
+++ student_practices/W11/D2/4-sequelize-validations-and-constraints/server/db/models/color.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Color extends Model {
     /**
@@ -12,15 +10,29 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Color.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  }
+  Color.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [2, 20],
+            msg: "Name must be between 2 and 20 characters long.",
+          },
+          noEndingInY(value) {
+            if (value.endsWith("y")) {
+              throw new Error("Name cannot end in 'y'.");
+            }
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Color",
     }
-  }, {
-    sequelize,
-    modelName: 'Color',
-  });
+  );
   return Color;
 };
