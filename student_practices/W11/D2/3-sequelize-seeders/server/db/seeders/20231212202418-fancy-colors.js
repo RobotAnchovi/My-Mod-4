f3c6a749341b8b23c855ff5cd82ef5dbe381b832
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-12 15:32:55 -0500

    Refactored Code in Practice 3

---
+++ student_practices/W11/D2/3-sequelize-seeders/server/db/seeders/20231212202418-fancy-colors.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const customDate = new Date("2023-01-01"); // Example date
+    await queryInterface.bulkInsert("Colors", [
+      { name: "aqua", createdAt: customDate, updatedAt: customDate },
+      { name: "fuchsia", createdAt: customDate, updatedAt: customDate },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Colors", {
+      name: { [Sequelize.Op.in]: ["aqua", "fuchsia"] },
+    });
+  },
+};
