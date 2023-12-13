b23864d2d26ce4c0b9965575d644fd2b698d57ff
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-13 17:49:55 -0500

    Completed final short practice

---
+++ student_practices/W11/D3/4-sequelize-foreign-key-migrations/server/db/migrations/20231213214616-add-bandId-to-musicians.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Musicians", "bandId", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Bands",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+      hooks: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Musicians", "bandId");
+  },
+};
--- student_practices/W11/D3/4-sequelize-foreign-key-migrations/server/db/models/musician.js
+++ student_practices/W11/D3/4-sequelize-foreign-key-migrations/server/db/models/musician.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Musician extends Model {
     /**
@@ -10,16 +8,29 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // Your code here 
+      Musician.belongsTo(models.Band, {
+        foreignKey: "bandId",
+        onDelete: "CASCADE",
+        hooks: true,
+      });
+
+      Musician.belongsToMany(models.Instrument, {
+        through: "MusicianInstrument",
+        foreignKey: "musicianId",
+        otherKey: "instrumentId",
+      });
     }
-  };
-  Musician.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    bandId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Musician',
-  });
+  }
+  Musician.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      bandId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Musician",
+    }
+  );
   return Musician;
 };
