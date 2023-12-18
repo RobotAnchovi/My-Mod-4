8e3dd4b3b4da4b4ec56fb1f827e6caefe1fc0f5f
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-18 16:49:22 -0500

    Add static file serving for scripts

--- student_practices/W12/D1/1-express-static-files/server/app.js
+++ student_practices/W12/D1/1-express-static-files/server/app.js
@@ -1,7 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// Your code here 
+// Part 1
+// app.use("/stylesheets", express.static("assets/css"));
+
+// Part 2
+// app.use(express.static("assets"));
+
+// Part 3
+app.use(express.static("assets/scripts"));
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
